ed7b8ce14518f386f7c029a6c3d3fd38f92a8b9a
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-08-13 11:47:59 -0600

    e

--- TM_CP3/Quiz_program/main.py
+++ TM_CP3/Quiz_program/main.py
@@ -4,28 +4,13 @@ import random
 import time
 import os
 
-def pystart(): # 
-        pygame.init()
-        # Set up the display
-        screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption('MTT Quiz')
-       
-        return(screen)
+
 
 #colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
-screen = pystart()
-
-def display(message, sec, x=50, y=50): # Displays a message on the screen by itself for a certain amount of seconds
-    font = pygame.font.Font(None, 36)
-    surface = font.render(message, True, (0, 0, 0))
-    screen.blit(surface, (x, y))
-    pygame.display.flip()  # Update the display
-    if sec != 0:
-        pygame.time.delay(sec * 1000)  # Waits a certain amount of seconds before continuing
 
 
 def load_questions(filename):
@@ -45,10 +30,10 @@ def load_questions(filename):
 
 
 def main():
-
+    screen = pygame.display.set_mode((1200, 800))
     pygame.init()
-    screen = pystart()
     screen.fill(BLACK)
+    pygame.display.set_caption('MTT Quiz')
     screen.blit(pygame.image.load("logo.png"), (0, 0))
     while True:
         for event in pygame.event.get():
