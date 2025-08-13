efe516a163b8d8e0186c8f86ecc3b2088c69b5d8
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-08-13 11:37:53 -0600

    ee

---
+++ TM_CP3/Quiz_program/main.py
@@ -0,0 +1,57 @@
+import pygame
+import csv
+import random
+import time
+
+def pystart(): # 
+        pygame.init()
+        # Set up the display
+        screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption('MTT Quiz')
+       
+        return(screen)
+
+#colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+screen = pystart()
+
+def display(message, sec, x=50, y=50): # Displays a message on the screen by itself for a certain amount of seconds
+    font = pygame.font.Font(None, 36)
+    surface = font.render(message, True, (0, 0, 0))
+    screen.blit(surface, (x, y))
+    pygame.display.flip()  # Update the display
+    if sec != 0:
+        pygame.time.delay(sec * 1000)  # Waits a certain amount of seconds before continuing
+
+
+def load_questions(filename):
+    questions = []
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            questions.append({
+                'topic': row['topic'],
+                'question': row['question'],
+                'options': [row['option1'], row['option2'], row['option3'], row['option4']],
+                'answer': row['answer']
+            })
+    return questions
+
+
+
+
+def main():
+
+    pygame.init()
+    screen = pystart()
+    screen.fill(BLACK)
+    screen.blit(pygame.image.load("mettaton_battle_box.webp"), (0, 0))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                raise SystemExit
+            
+main()
