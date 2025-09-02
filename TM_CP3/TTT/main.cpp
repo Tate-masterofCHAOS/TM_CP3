0be0b28b74bf57bb11b80ee0a47074199f34bae3
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-09-02 11:48:39 -0600

    worked on tic tac toe

--- TM_CP3/TTT/main.cpp
+++ TM_CP3/TTT/main.cpp
@@ -7,8 +7,8 @@ using namespace std;
 string board[3][3] = {{"1","2","3"},
                         {"4","5","6"},
                         {"7","8","9"}};
-
-
+bool game = true;
+string winner = "";
 
 int display_board(){
     for (int i=0;i<3;i++){
@@ -36,13 +36,81 @@ int player_turn(){
 }
 
 int comp_turn(){
+    cout << endl << endl << endl << endl;
     int seconds = time(nullptr);
     srand(seconds);
-    int my_num = rand() % 11;
-    cout << my_num << endl;
+    int my_num = rand() % 10;
+    string comp_choice = "2";
+    if(my_num == 0){
+        comp_choice = "1";
+    }
+    else if(my_num == 1){
+        comp_choice = "2";
+    }
+    else if(my_num == 2){
+        comp_choice = "3";
+    }
+    else if(my_num == 3){
+        comp_choice = "4";
+    }
+    else if(my_num == 4){
+        comp_choice = "5";
+    }
+    else if(my_num == 5){
+        comp_choice = "6";
+    }
+    else if(my_num == 6){
+        comp_choice = "7";
+    }
+    else if(my_num == 7){
+        comp_choice = "8";
+    }
+    else if(my_num == 8){
+        comp_choice = "9";
+    }
+
+    for (int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            if(board[i][j] == comp_choice){
+                board[i][j] = "O";
+            }
+        }
+    }
+    return 0;
+}
+
+int game_check(){
+    //check rows
+    for (int i=0;i<3;i++){
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+            game = false;
+            winner = board[i][0];
+        }
+    }
+    //check columns
+    for (int i=0;i<3;i++){
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i]){
+            game = false;
+            winner = board[0][i];
+        }
+    }
+    //check diagonals
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+        game = false;
+        winner = board[0][0];
+    }
     return 0;
 }
 int main(){
-    comp_turn();
+    while (game = true){
+        display_board();
+        player_turn();
+        comp_turn();
+        game_check();
+        if (game == false){
+            break;
+        }
+    }
+    cout << endl << "WINNER IS " << winner;
     
 }
---
+++ TM_CP3/TTT/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+
+    else if(my_num = 2){
+        string comp_choice = "3";
+    }
+    else if(my_num = 3){
+        string comp_choice = "4";
+    }
