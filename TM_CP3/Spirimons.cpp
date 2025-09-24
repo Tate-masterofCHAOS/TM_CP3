0d386a8cd59bc8d005880a63093c823e2b46ad4c
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-09-24 11:48:54 -0600

    wprlked on pokemon

--- TM_CP3/Spirimons.cpp
+++ TM_CP3/Spirimons.cpp
@@ -10,158 +10,233 @@
 using namespace Gdiplus;
 using namespace std;  
 
+bool feld = false;
 struct Mon{ 
     string name;
-    int ATK;
     int DEF;
     int SPD;
     int agility;
+    int health;
     string title;
     string type;
     string dualType = "none";
     string desc;
+    string attack1;
+    string attack2;
+    string attack3;
+    string attack4;
+    int attack1power;
+    int attack2power;
+    int attack3power;
+    int attack4power;
     int mondex(){
-        MessageBoxA(NULL, (name + " " + title + " a " + type + " and " + dualType + " type\n\n" + desc).c_str(), "Mon Dex Entry", MB_OK | MB_ICONINFORMATION);
+        cout << name << " " << title << " a " << type << " and " << dualType << " type\n\n" << desc << endl;
         return 0;
     }
+};           
+
+struct party{
+    Mon mona;
+    Mon monb;
+    Mon monc;
+    Mon mond;
+    Mon mone;
+    Mon monf;
+
+    int player_turn(Mon opponent, party party){
+        cout << "What do you do?" << endl;
+        cout << "1. Attack" << endl;
+        cout << "2. Heal" << endl;
+        cout << "3. Catch" << endl;
+        cout << "4. Run" << endl;
+        string action;
+        cin >> action;
+        if (action == "1") {
+            cout << "what attack?" << endl;
+            cout << "1. " << mona.attack1 << endl;
+            cout << "2. " << mona.attack2 << endl;
+            cout << "3. " << mona.attack3 << endl;
+            cout << "4. " << mona.attack4 << endl;
+            cin >> action;
+            if (action == "1") {
+                cout << mona.name << " used " << mona.attack1 << "!" << endl;
+                cout << opponent.name << " took " << mona.attack1power - opponent.DEF << " damage!" << endl;
+                opponent.health -= (mona.attack1power - opponent.DEF);
+            } else if (action == "2") {
+                cout << mona.name << " used " << mona.attack2 << "!" << endl;
+                cout << opponent.name << " took " << mona.attack2power - opponent.DEF << " damage!" << endl;
+                opponent.health -= (mona.attack2power - opponent.DEF);
+            } else if (action == "3") {
+                cout << mona.name << " used " << mona.attack3 << "!" << endl;
+                cout << opponent.name << " took " << mona.attack3power - opponent.DEF << " damage!" << endl;
+                opponent.health -= (mona.attack3power - opponent.DEF);
+            } else if (action == "4") {
+                cout << mona.name << " used " << mona.attack4 << "!" << endl;
+                cout << opponent.name << " took " << mona.attack4power - opponent.DEF << " damage!" << endl;
+                opponent.health -= (mona.attack4power - opponent.DEF);
+            } else {
+                cout << "invalid choice, you lose your turn!" << endl;
+            }
+        } else if (action == "2") {
+            cout << mona.name << " healed 10 health!" << endl;
+            mona.health += 10;
+        } else if (action == "3") {
+            cout << "You threw a pokeball!" << endl;
+            int catch_chance = rand() % 100;
+            if (catch_chance < 50) {
+                cout << "You caught " << opponent.name << "!" << endl;
+                if (party.mona.name == "") {
+                    party.mona = opponent;
+                } else if (party.monb.name == "") {
+                    party.monb = opponent;
+                } else if (party.monc.name == "") {
+                    party.monc = opponent;
+                } else if (party.mond.name == "") {
+                    party.mond = opponent;
+                } else if (party.mone.name == "") {
+                    party.mone = opponent;
+                } else if (party.monf.name == "") {
+                    party.monf = opponent;
+                } else {
+                    cout << "Your party is full, you cannot catch more mon!" << endl;
+                }
+            } else {
+                cout << opponent.name << " broke free!" << endl;
+            }
+        } else if (action == "4") {
+            cout << "You ran away!" << endl;
+            bool feld = true;
+            return feld;
+        }
+    return 0;
+    }
+    int opponent_turn(Mon opponent, party party){
+        int action = rand() % 2 + 1;
+        if (action == 1) {
+            int attack = rand() % 4 + 1;
+            if (attack == 1) {
+                cout << opponent.name << " used " << opponent.attack1 << "!" << endl;
+                cout << mona.name << " took " << opponent.attack1power - mona.DEF << " damage!" << endl;
+                mona.health -= (opponent.attack1power - mona.DEF);
+            } else if (attack == 2) {
+                cout << opponent.name << " used " << opponent.attack2 << "!" << endl;
+                cout << mona.name << " took " << opponent.attack2power - mona.DEF << " damage!" << endl;
+                mona.health -= (opponent.attack2power - mona.DEF);
+            } else if (attack == 3) {
+                cout << opponent.name << " used " << opponent.attack3 << "!" << endl;
+                cout << mona.name << " took " << opponent.attack3power - mona.DEF << " damage!" << endl;
+                mona.health -= (opponent.attack3power - mona.DEF);
+            } else if (attack == 4) {
+                cout << opponent.name << " used " << opponent.attack4 << "!" << endl;
+                cout << mona.name << " took " << opponent.attack4power - mona.DEF << " damage!" << endl;
+                mona.health -= (opponent.attack4power - mona.DEF);
+            }
+        } else if (action == 2) {
+            cout << opponent.name << " healed 10 health!" << endl;
+            opponent.health += 10;
+        }
+        return 0;
+    }
+    int battle_mon(Mon opponent, party party){
+        cout << "A battle has started between " << mona.name << " and " << monb.name << "!" << endl;
+        while (mona.health > 0 && opponent.health > 0 && !feld) {
+            if (mona.SPD >= opponent.SPD) {
+                player_turn(opponent, party);
+                if (opponent.health <= 0) {
+                    cout << opponent.name << " fainted!" << endl;
+                    break;
+                }
+                opponent_turn(opponent, party);
+                if (mona.health <= 0) {
+                    cout << mona.name << " fainted!" << endl;
+                    break;
+                }
+            } else {
+                opponent_turn(opponent, party);
+                if (mona.health <= 0) {
+                    cout << mona.name << " fainted!" << endl;
+                    break;
+                }
+                player_turn(opponent, party);
+                if (opponent.health <= 0) {
+                    cout << opponent.name << " fainted!" << endl;
+                    break;
+                }
+        }
+        
+        
+        return 0;
+    }   
+
 };
 
-struct Player{
-    Mon mon;
-};
-LRESULT CALLBACK DialogProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    switch (msg) {
-        case WM_COMMAND:
-            switch (LOWORD(wParam)) {
-                case 1: // Button 1
-                    MessageBox(hwnd, "You clicked Option 1!", "Info", MB_OK);
-                    EndDialog(hwnd, 1);
-                    return TRUE;
-                case 2: // Button 2
-                    MessageBox(hwnd, "You clicked Option 2!", "Info", MB_OK);
-                    EndDialog(hwnd, 2);
-                    return TRUE;
-                case 3: // Button 3
-                    MessageBox(hwnd, "You clicked Option 3!", "Info", MB_OK);
-                    EndDialog(hwnd, 3);
-                    return TRUE;
-            }
+int main() {
+    Mon mon0 = {"Larose",5, 5, 5, 5, "", "Normal", "Fae", "Youre adding descriptions to them?", "Sarcasm", "Lightsaber", "Bad grade", "Tangent", 5, 5, 999, 0};
+    Mon mon1 = {"Wanderer", 5,5, 5, 5, "The lost man mon", "Spirit", "Light", "After spending most of their life in haunted areas has infused them with many curses and possesions but unlike many spirit types they are uneffected to light and will use flashlights to blind it's prey before attacking", "Splish", "Splash", "Sploosh", "Splesh", 5, 5, 5, 5};
+    Mon mon2 = {"George", 5,5, 5, 5, "The tickler mon", "Spirit", "Humor", "While slow, they get stronger the more damage they have done. To stun their prey they will tickle...that's it", "Tickle", "Clang", "Deciever deciever I shall cut thy pants with a cleaver", "", 5, 5, 5, 5};
+    Mon mon3 = {"Flatline", 5,5, 5, 5, "The two dimensional mon", "Spirit", "Humor", "Ever wonder who is at fault, it is this mon's fault, they are reprehensible", "Blame", "Throw", "Slide", "Slash", 5, 5, 5, 5};
+    Mon mon4 = {"Random fae in a bottle", 5,5, 5, 5, "The name stealer mon", "Fae", "Spirit", "Dave. There are many daves in the world, it is the fault of this mon, with a nasty habit of stealing and replacing names of people they meet they have many names but everyone they meet only has one, Dave.", "Davifiy", "Steal", "Float", "Screech", 5, 5, 5, 5};
+    Mon mon5 = {"Abraham", 5, 5,5, 5, "The presidential mon", "Political", "Spirit", "Anybody who has been within the white house would know it is unnaturally cold, the reason for that is this mon always watching over his successors and making sure the ones he doesnt like dont get re-elected", "Mine", "Moon", "Men", "man", 5, 5, 5, 5};
+    Mon mon6 = {"Chester", 5,5, 5, 5, "The strongest climbing mon", "Climber", "Swole", "The only living mon left, it spends eternity climbing random ledges", "climb", "scuttle", "jump", "hook", 5, 5, 5, 5};
+    Mon mon7 = {"Togore", 5, 5,5, 5, "The golfer mon", "Golfer", "Stupoid", "Plays golf all day everyday which is stupid, golf is stupid", "hole in one", "birdie", "hole in three", "hole in two", 5, 5, 5, 5};
+
+    cout << "Welcome to Spirimons!" << endl << "please choose a starter" << endl << endl;
+
+    cout << "1. "; mon1.mondex(); cout << endl;
+    cout << "2. "; mon2.mondex(); cout << endl;
+    cout << "3. "; mon3.mondex(); cout << endl;
+    string monelection;
+    cin >> monelection;
+    party p;
+    if(monelection == "1") {
+        p.mona = mon1;
+    } else if (monelection == "2") {
+        p.mona = mon2;
+    } else if (monelection == "3") {
+        p.mona = mon3;
+    } else {
+        cout << "invalid choice, defaulting to Luke" << endl;
+        p.mona = mon1;
+    }
+    cout << "You chose " << p.mona.name << " the " << p.mona.title << "!" << endl;
+    cout << "Your rival, Barry has chosen " << mon4.name << " the " << mon4.title << "!" << endl;
+    while (true) {
+        cout << "What would you like to do?" << endl;
+        cout << "1. Fight Barry" << endl;
+        cout << "2. Find a wild mon" << endl;
+        cout << "3. Check your mon's stats" << endl;
+        cout << "4. Heal your mon" << endl;
+        cout << "5. Exit game" << endl;
+        string choice;
+        cin >> choice;
+        if (choice == "1") {
+            cout << "You chose to fight Barry!" << endl;
+            //battle function here
+        } else if (choice == "2") {
+            cout << "You chose to find a wild mon!" << endl;
+            Mon opponent = mon5; //wild encounter function here
+            p.battle_mon(opponent, p);
+        } else if (choice == "3") {
+            cout << "You chose to check your mon's stats!" << endl;
+            cout << p.mona.name << " the " << p.mona.title << endl;
+            cout << "Type: " << p.mona.type << " / " << p.mona.dualType << endl;
+            cout << "DEF: " << p.mona.DEF << endl;
+            cout << "SPD: " << p.mona.SPD << endl;
+            cout << "Agility: " << p.mona.agility << endl;
+            cout << "Health: " << p.mona.health << endl;
+            cout << "Attacks: " << p.mona.attack1 << ", " << p.mona.attack2 << ", " << p.mona.attack3 << ", " << p.mona.attack4 << endl;
+        } else if (choice == "4") {
+            cout << "You chose to heal your mon!" << endl;
+            //healing function here
+        } else if (choice == "5") {
+            cout << "Exiting game. Goodbye!" << endl;
             break;
-        case WM_CLOSE:
-            EndDialog(hwnd, 0);
-            return TRUE;
-    }
-    return FALSE;
-}
-// Window procedure function
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    Player user;
-    switch (msg) {
-        case WM_COMMAND:
-            switch (LOWORD(wParam)) {
-                case 1: // Button 1 clicked
-                    user.mon = {"Nick", 5, 5, 5, 5, "The tickler mon", "Zombie", "Humor", "While slow, they get stronger the more damage they have done. To stun their prey they will tickle...that's it"};
-                    user.mon.mondex();
-                    // Or call your custom function here
-                    break;
-                case 2: // Button 2 clicked
-                    user.mon = {"Luke", 5, 5, 5, 5, "The man mon", "Spirit", "Light", "After spending most of their life in haunted areas has infused them with many curses and possesions but unlike many spirit types they are uneffected to light and will use flashlights to blind it's prey before attacking"};
-                    user.mon.mondex();
-                    break;
-                case 3: // Button 3 clicked
-                    user.mon = {"Cecily", 5, 5, 5, 5, "The name stealer mon", "Fae", "Trickster", "Dave. There are many daves in the world, it is the fault of this mon, with a nasty habit of stealing and replacing names of people they meet they have many names but everyone they meet only has one, Dave."};
-                    user.mon.mondex();
-                    break;
-                case 4: // Button 4 clicked
-                    user.mon = {"Johnas", 5, 5, 5, 5, "The two dimensional mon", "Poletgiest", "Reprehensible", "Ever wonder who is at fault, it is this mon's fault, they are reprehensible"};
-                    user.mon.mondex();
-                    break;
-                case 5: // Button 5 clicked
-                    user.mon = {"Darius", 5, 5, 5, 5, "The presidential mon", "Political", "Spirit", "Anybody who has been within the white house would know it is unnaturally cold, the reason for that is this mon always watching over his successors and making sure the ones he doesnt like dont get re-elected"};
-                    user.mon.mondex();
-                    break;
-                case 6: // Button 6 clicked
-                    user.mon = {"Vincent", 5, 5, 5, 5, "The strongest climbing mon", "Climber", "Swole", "The only living humon left, it spends eternity climbing random ledges"};
-                    user.mon.mondex();
-                    break;
-                case 7: // Button 7 clicked
-                    user.mon = {"Voncent", 5, 5, 5, 5, "The golfer mon", "Golfer", "Stupoid", "Plays golf all day everyday which is stupid, golf is stupid"};
-                    user.mon.mondex();
-                    break;
-                // Add more cases for more buttons
-            }
+        } else {
+            cout << "Invalid choice, exiting game." << endl;
             break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-    }
-    return DefWindowProc(hwnd, msg, wParam, lParam);
-}
-
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
-    Mon mon0 = {"Larose", 5, 5, 5, 5, "", "Normal", "Fae", "Youre adding descriptions to them?"};
-    Mon mon1 = {"Luke", 5, 5, 5, 5, "The lost man mon", "Spirit", "Light", "After spending most of their life in haunted areas has infused them with many curses and possesions but unlike many spirit types they are uneffected to light and will use flashlights to blind it's prey before attacking"};
-    Mon mon2 = {"Nick", 5, 5, 5, 5, "The tickler mon", "Zombie", "Humor", "While slow, they get stronger the more damage they have done. To stun their prey they will tickle...that's it"};
-    Mon mon3 = {"Johnas", 5, 5, 5, 5, "The two dimensional mon", "Poletgiest", "Reprehensible", "Ever wonder who is at fault, it is this mon's fault, they are reprehensible"};
-    Mon mon4 = {"Cecily", 5, 5, 5, 5, "The name stealer mon", "Fae", "Trickster", "Dave. There are many daves in the world, it is the fault of this mon, with a nasty habit of stealing and replacing names of people they meet they have many names but everyone they meet only has one, Dave."};
-    Mon mon5 = {"Darius", 5, 5, 5, 5, "The presidential mon", "Political", "Spirit", "Anybody who has been within the white house would know it is unnaturally cold, the reason for that is this mon always watching over his successors and making sure the ones he doesnt like dont get re-elected"};
-    Mon mon6 = {"Vincent", 5, 5, 5, 5, "The strongest climbing mon", "Climber", "Swole", "The only living mon left, it spends eternity climbing random ledges"};
-    Mon mon7 = {"Voncent", 5, 5, 5, 5, "The golfer mon", "Golfer", "Stupoid", "Plays golf all day everyday which is stupid, golf is stupid"};
-    const char CLASS_NAME[] = "CustomButtonWindow";
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-
-    RegisterClass(&wc);
-
-    HWND hwnd = CreateWindowEx(
-        0, CLASS_NAME, "Custom Options",
-        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 5000, 5000,
-        NULL, NULL, hInstance, NULL
-    );
-
-    if (hwnd == NULL) return 0;
-
-    // Create three buttons
-    CreateWindow("BUTTON", "Nick", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-        1000, 500, 100, 30, hwnd, (HMENU)1, hInstance, NULL);
-    CreateWindow("BUTTON", "Luke", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-        1100, 500, 100, 30, hwnd, (HMENU)2, hInstance, NULL);
-    CreateWindow("BUTTON", "Cecily", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-        1200, 500, 100, 30, hwnd, (HMENU)3, hInstance, NULL);
-    CreateWindow("BUTTON", "Johnas", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-        1000, 530, 100, 30, hwnd, (HMENU)4, hInstance, NULL);
-    CreateWindow("BUTTON", "Darius", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-        1100, 530, 100, 30, hwnd, (HMENU)5, hInstance, NULL);
-    CreateWindow("BUTTON", "Vincent", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-        1200, 530, 100, 30, hwnd, (HMENU)6, hInstance, NULL);
-    CreateWindow("BUTTON", "Voncent", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-        1300, 530, 100, 30, hwnd, (HMENU)7, hInstance, NULL);
-    ShowWindow(hwnd, nCmdShow);
-
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        }
     }
 
-
-    //Battle system here
-    CreateWindowEx(
-        0, CLASS_NAME, "Battle System",
-        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 5000, 5000,
-        NULL, NULL, hInstance, NULL
-    );
-    if (hwnd == NULL) return 0;
-    ShowWindow(hwnd, nCmdShow);
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-    DestroyWindow(hwnd);
-    
     return 0;
 }
     
