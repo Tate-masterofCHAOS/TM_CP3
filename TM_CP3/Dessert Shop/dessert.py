a2d047ff21dadb5ce2d2f1c4d45f479828c5b8bf
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-10-21 11:48:54 -0600

    worked on dessert shop

--- TM_CP3/Dessert Shop/dessert.py
+++ TM_CP3/Dessert Shop/dessert.py
@@ -1,34 +1,43 @@
 class DessertItem:
-    def __init__(self,name):
+    name: str
+    def __init__(self,name=""):
         self.name = name
 
-class Candy:
-    def __init__(self, name, price_per_pound, amount):
+class Candy(DessertItem):
+    name: str 
+    weight: float 
+    price_per_pound: float 
+    def __init__(self, name, weight=0.0, price_per_pound=0.0):
         super.__init__(name)
-        self.name = name
         self.price_per_pound = price_per_pound
-        self.amount= amount
+        self.weight= weight
 
-class IceCream:
-    def __init__(self, name, price_per_scoop, scoops):
+class Cookie(DessertItem):
+    name: str 
+    cookie_quantity: int 
+    price_per_pound: float 
+    def __init__(self, name, cookie_quantity=0, price_per_dozen=0.0):
         super.__init__(name)
-        self.name = name
-        self.price_per_scoop = price_per_scoop
-        self.scoops = scoops
-
-class Cookies:
-    def __init__(self, name, price_per_dozen, amount_of_cookies):
-        super.__init__(name)
-        self.name = name
         self.price_per_dozen = price_per_dozen
-        self.amount_of_cookies = amount_of_cookies
+        self.cookie_quantity = cookie_quantity
 
-class Sundae:
-    def __init__(self, name, price_per_scoop, scoops, topping_name, topping_price):
-        super.__init__(name,price_per_scoop,scoops)
-        self.name = name
+class IceCream(DessertItem):
+    name: str 
+    scoop_count: int 
+    price_per_scoop: float 
+    def __init__(self, name, scoop_count, price_per_scoop):
+        super.__init__(name)
         self.price_per_scoop = price_per_scoop
-        self.scoops = scoops
+        self.scoop_count = scoop_count
+
+class Sundae(IceCream):
+    name: str 
+    scoop_count: int 
+    price_per_scoop: float 
+    topping_name: str 
+    topping_price: float 
+    def __init__(self, name, scoop_count, price_per_scoop, topping_name, topping_price):
+        super.__init__(name,price_per_scoop,scoop_count)
         self.topping_name = topping_name
         self.topping_price = topping_price
 
@@ -38,5 +47,5 @@ class order:
     
     def add(DessertItem):
         order.append(DessertItem)
-    def __len__():
-        pass
+    def __len__(self):
+        return len(self.order)
--- TM_CP3/Dessert Shop/dessert_shop.py
+++ TM_CP3/Dessert Shop/dessert_shop.py
@@ -2,4 +2,17 @@ from dessert import *
 
 def main():
     Joes_order = order()
-    Joes_order.add("Gummy Bears", .34, 1.25)
+    Joes_order(Candy("Candy Corn", 1.5, .25))
+    Joes_order(Candy("Gummy Bears", .25, .35))
+    Joes_order(Cookie("Chocolate Chip", 6, 3.99))
+    Joes_order(IceCream("Pistachio", 2, .79))
+    Joes_order(Sundae("Vanilla", 3, .69, "Hot Fudge", 1.29))
+    Joes_order(Cookie("Oatmeal Raisin", 2, 3.45))
+    print("Joe's Dessert Order")
+    print("---------------------")
+    for item in Joes_order.order:
+        print(item.name)
+    print("---------------------")
+    print(f"Number of items: {len(Joes_order)}")
+
+main()
