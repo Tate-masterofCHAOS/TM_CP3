bba6305da90403ba97cb707afa7d532ba6f3ab13
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-10-23 11:21:07 -0600

    finished desert shop

--- TM_CP3/Dessert Shop/dessert.py
+++ TM_CP3/Dessert Shop/dessert.py
@@ -8,7 +8,7 @@ class Candy(DessertItem):
     weight: float 
     price_per_pound: float 
     def __init__(self, name, weight=0.0, price_per_pound=0.0):
-        super.__init__(name)
+        super().__init__(name)
         self.price_per_pound = price_per_pound
         self.weight= weight
 
@@ -17,7 +17,7 @@ class Cookie(DessertItem):
     cookie_quantity: int 
     price_per_pound: float 
     def __init__(self, name, cookie_quantity=0, price_per_dozen=0.0):
-        super.__init__(name)
+        super().__init__(name)
         self.price_per_dozen = price_per_dozen
         self.cookie_quantity = cookie_quantity
 
@@ -25,8 +25,8 @@ class IceCream(DessertItem):
     name: str 
     scoop_count: int 
     price_per_scoop: float 
-    def __init__(self, name, scoop_count, price_per_scoop):
-        super.__init__(name)
+    def __init__(self, name, scoop_count=0, price_per_scoop=0.0):
+        super().__init__(name)
         self.price_per_scoop = price_per_scoop
         self.scoop_count = scoop_count
 
@@ -36,8 +36,8 @@ class Sundae(IceCream):
     price_per_scoop: float 
     topping_name: str 
     topping_price: float 
-    def __init__(self, name, scoop_count, price_per_scoop, topping_name, topping_price):
-        super.__init__(name,price_per_scoop,scoop_count)
+    def __init__(self, name, scoop_count=0, price_per_scoop=0.0, topping_name="", topping_price=0.0):
+        super().__init__(name,price_per_scoop,scoop_count)
         self.topping_name = topping_name
         self.topping_price = topping_price
 
@@ -45,7 +45,7 @@ class order:
     def __init__(self):
         self.order = []
     
-    def add(DessertItem):
-        order.append(DessertItem)
+    def add(self, DessertItem):
+        self.order.append(DessertItem)
     def __len__(self):
         return len(self.order)
--- TM_CP3/Dessert Shop/dessert_shop.py
+++ TM_CP3/Dessert Shop/dessert_shop.py
@@ -2,12 +2,12 @@ from dessert import *
 
 def main():
     Joes_order = order()
-    Joes_order(Candy("Candy Corn", 1.5, .25))
-    Joes_order(Candy("Gummy Bears", .25, .35))
-    Joes_order(Cookie("Chocolate Chip", 6, 3.99))
-    Joes_order(IceCream("Pistachio", 2, .79))
-    Joes_order(Sundae("Vanilla", 3, .69, "Hot Fudge", 1.29))
-    Joes_order(Cookie("Oatmeal Raisin", 2, 3.45))
+    Joes_order.add(Candy("Candy Corn", 1.5, .25))
+    Joes_order.add(Candy("Gummy Bears", .25, .35))
+    Joes_order.add(Cookie("Chocolate Chip", 6, 3.99))
+    Joes_order.add(IceCream("Pistachio", 2, .79))
+    Joes_order.add(Sundae("Vanilla", 3, .69, "Hot Fudge", 1.29))
+    Joes_order.add(Cookie("Oatmeal Raisin", 2, 3.45))
     print("Joe's Dessert Order")
     print("---------------------")
     for item in Joes_order.order:
