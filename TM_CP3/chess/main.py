ef66d3a5de9308d65606e8eb954855264592dff7
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-10-27 11:12:02 -0600

    finsihed cgess

--- TM_CP3/chess/main.py
+++ TM_CP3/chess/main.py
@@ -1,56 +1,98 @@
 from classes import *
 
-def board(pawnsW, rooksW, knightsW, bishopsW, queenW, kingW):
-    Line1 = ["▢","■","▢","■","▢","■","▢","■"]
+def board(pawnsW, rooksW, knightsW, bishopsW, queenW, kingW, pawnsB, rooksB, knightsB, bishopsB, queenB, kingB):
+    Line1 = ["■","▢","■","▢","■","▢","■","▢"]
     line2 = ["▢","■","▢","■","▢","■","▢","■"]
-    line3 = ["▢","■","▢","■","▢","■","▢","■"]
+    line3 = ["■","▢","■","▢","■","▢","■","▢"]
     line4 = ["▢","■","▢","■","▢","■","▢","■"]
-    line5 = ["▢","■","▢","■","▢","■","▢","■"]
+    line5 = ["■","▢","■","▢","■","▢","■","▢"]
     line6 = ["▢","■","▢","■","▢","■","▢","■"]
-    line7 = ["▢","■","▢","■","▢","■","▢","■"]
+    line7 = ["■","▢","■","▢","■","▢","■","▢"]
     line8 = ["▢","■","▢","■","▢","■","▢","■"]
 
     for pawn in pawnsW:
         if pawn.position == "A2":
-            line7[0] = pawn.getSymbol()
+            line2[0] = pawn.getSymbol()
         elif pawn.position == "B2":
-            line7[1] = pawn.getSymbol()
+            line2[1] = pawn.getSymbol()
         elif pawn.position == "C2":
-            line7[2] = pawn.getSymbol()
+            line2[2] = pawn.getSymbol()
         elif pawn.position == "D2":
-            line7[3] = pawn.getSymbol()
+            line2[3] = pawn.getSymbol()
         elif pawn.position == "E2":
-            line7[4] = pawn.getSymbol()
+            line2[4] = pawn.getSymbol()
         elif pawn.position == "F2":
-            line7[5] = pawn.getSymbol()
+            line2[5] = pawn.getSymbol()
         elif pawn.position == "G2":
-            line7[6] = pawn.getSymbol()
+            line2[6] = pawn.getSymbol()
         elif pawn.position == "H2":
-            line7[7] = pawn.getSymbol()
+            line2[7] = pawn.getSymbol()
 
     for rook in rooksW:
         if rook.position == "A1":
-            line8[0] = rook.getSymbol()
+            Line1[0] = rook.getSymbol()
         elif rook.position == "H1":
-            line8[7] = rook.getSymbol()
+            Line1[7] = rook.getSymbol()
 
     for knight in knightsW:
         if knight.position == "B1":
-            line8[1] = knight.getSymbol()
+            Line1[1] = knight.getSymbol()
         elif knight.position == "G1":
-            line8[6] = knight.getSymbol()
+            Line1[6] = knight.getSymbol()
     
     for bishop in bishopsW:
         if bishop.position == "C1":
-            line8[2] = bishop.getSymbol()
+            Line1[2] = bishop.getSymbol()
         elif bishop.position == "F1":
-            line8[5] = bishop.getSymbol()
+            Line1[5] = bishop.getSymbol()
     
     if queenW.position == "D1":
-        line8[3] = queenW.getSymbol()
+        Line1[3] = queenW.getSymbol()
     
     if kingW.position == "E1":
-        line8[4] = kingW.getSymbol()
+        Line1[4] = kingW.getSymbol()
+
+    for pawn in pawnsB:
+        if pawn.position == "A7":
+            line7[0] = pawn.getSymbol()
+        elif pawn.position == "B7":
+            line7[1] = pawn.getSymbol()
+        elif pawn.position == "C7":
+            line7[2] = pawn.getSymbol()
+        elif pawn.position == "D7":
+            line7[3] = pawn.getSymbol()
+        elif pawn.position == "E7":
+            line7[4] = pawn.getSymbol()
+        elif pawn.position == "F7":
+            line7[5] = pawn.getSymbol()
+        elif pawn.position == "G7":
+            line7[6] = pawn.getSymbol()
+        elif pawn.position == "H7":
+            line7[7] = pawn.getSymbol()
+
+    for rook in rooksB:
+        if rook.position == "A8":
+            line8[0] = rook.getSymbol()
+        elif rook.position == "H8":
+            line8[7] = rook.getSymbol()
+
+    for knight in knightsB:
+        if knight.position == "B8":
+            line8[1] = knight.getSymbol()
+        elif knight.position == "G8":
+            line8[6] = knight.getSymbol()
+    
+    for bishop in bishopsB:
+        if bishop.position == "C8":
+            line8[2] = bishop.getSymbol()
+        elif bishop.position == "F8":
+            line8[5] = bishop.getSymbol()
+    
+    if queenB.position == "D8":
+        line8[3] = queenB.getSymbol()
+    
+    if kingB.position == "E8":
+        line8[4] = kingB.getSymbol()
 
     print(f"{Line1}\n{line2}\n{line3}\n{line4}\n{line5}\n{line6}\n{line7}\n{line8}\n")
 
@@ -82,6 +124,32 @@ def main():
 
     kingW = King("White", "E1")
 
-    board(pawnsW, rooksW, knightsW, bishopsW, queenW, kingW)
+    pawn1B = Pawn("Black", "A7")
+    pawn2B = Pawn("Black", "B7")
+    pawn3B = Pawn("Black", "C7")
+    pawn4B = Pawn("Black", "D7")
+    pawn5B = Pawn("Black", "E7")
+    pawn6B = Pawn("Black", "F7")
+    pawn7B = Pawn("Black", "G7")
+    pawn8B = Pawn("Black", "H7")
+    pawnsB = [pawn1B, pawn2B, pawn3B, pawn4B, pawn5B, pawn6B, pawn7B, pawn8B]
+
+    rook1B = Rook("Black", "A8")
+    rook2B = Rook("Black", "H8")
+    rooksB = [rook1B, rook2B]
+
+    knight1B = Knight("Black", "B8")
+    knight2B = Knight("Black", "G8")
+    knightsB = [knight1B, knight2B]
+
+    bishops1B = Bishop("Black", "C8")
+    bishop2B = Bishop("Black", "F8")
+    bishopsB = [bishops1B, bishop2B]
+
+    queenB = Queen("Black", "D8")
+
+    kingB = King("Black", "E8")
+
+    board(pawnsW, rooksW, knightsW, bishopsW, queenW, kingW, pawnsB, rooksB, knightsB, bishopsB, queenB, kingB)
 
 main()
