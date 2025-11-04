1a82d90655b75d50633031ef36246ff1c70b7f88
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-11-04 08:18:51 -0700

    finished calculator

--- TM_CP3/calcalatar.cpp
+++ TM_CP3/calcalatar.cpp
@@ -1,69 +1,70 @@
-//TM CP3 calculator
-
 #include <iostream>
 
-using namespace std;  
+using namespace std;
 
-enum choice{
+enum choiceMenu {
     Add = 1,
     Subtract,
     Multiply,
     Divide,
     Leave,
-},
+};
 
-int add(num1, num2){
-    num1 = num1 + num2;
-    return num1;
-}
+double addition(double a, double b) { return a + b; }
+double subtraction(double a, double b) { return a - b; }
+double multiplication(double a, double b) { return a * b; }
+double division(double a, double b) { return a / b; }
 
-int subtract(num1, num2){
-    num1 = num1 - num2;
-    return num1;
-}
-
-int multiply(num1, num2){
-    num1 = num1 * num2;
-    return num1;
-}
-
-int divide(num1, num2){
-    num1 = num1 / num2;
-    return num1;
-}
-
-int main(){
+int main() {
     bool running = true;
-    while running{
-        int num1;
-        int num2;
-        cout << "Number 1";
-        cin >> num1;
-        cout << "Number 2";
-        cin >> num2
-        cout << "What would you like to do?" << endl;
-        cout << "1. Add" << endl;
-        cout << "2. Subtract" << endl;
-        cout << "3. Multiply" << endl;
-        cout << "4. Divide" << endl;
-        cout << "5. Exit game" << endl;
-        int choice;
-        cin >> choice;
-        if (choice == choiceMenu::Add) {
-            add(num1, num2);
+    while (running) {
+        double num1 = 0.0;
+        double num2 = 0.0;
+        cout << "Number 1: ";
+        if (!(cin >> num1)) { cerr << "Invalid input\n"; return 1; }
+        cout << "Number 2: ";
+        if (!(cin >> num2)) { cerr << "Invalid input\n"; return 1; }
+
+        cout << "What would you like to do?\n";
+        cout << "1. Add\n";
+        cout << "2. Subtract\n";
+        cout << "3. Multiply\n";
+        cout << "4. Divide\n";
+        cout << "5. Exit\n";
+        int choice = 0;
+        if (!(cin >> choice)) { cerr << "Invalid choice\n"; return 1; }
+
+        double result = 0.0;
+        switch (choice) {
+            case choiceMenu::Add:
+                result = addition(num1, num2);
+                cout << "Result: " << result << '\n';
+                break;
+            case choiceMenu::Subtract:
+                result = subtraction(num1, num2);
+                cout << "Result: " << result << '\n';
+                break;
+            case choiceMenu::Multiply:
+                result = multiplication(num1, num2);
+                cout << "Result: " << result << '\n';
+                break;
+            case choiceMenu::Divide:
+                if (num2 == 0.0) {
+                    cout << "Error: division by zero\n";
+                } else {
+                    result = division(num1, num2);
+                    cout << "Result: " << result << '\n';
+                }
+                break;
+            case choiceMenu::Leave:
+                running = false;
+                break;
+            default:
+                cout << "Unknown choice\n";
         }
-        if (choice == choiceMenu::Subtract) {
-            subtract(num1, num2);
-        }
-        if (choice == choiceMenu::Multiply) {
-            multiply(num1, num2);
-        }
-        if (choice == choiceMenu::Divide) {
-            divide(num1, num2);
-        }
-        if (choice == choiceMenu::Leave) {
-            running = false;
-        }
+
+        cout << '\n';
     }
-    
+
+    return 0;
 }
