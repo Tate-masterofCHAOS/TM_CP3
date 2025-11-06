04e000b4fd86402eeb4a52ef4e7c249b6ceabeb0
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-11-06 01:54:22 +0000

    did pygame notes

---
+++ Notes/pygame_notes.py
@@ -0,0 +1,20 @@
+#Tm CP3 Pygame Notes
+
+'''
+How do you set up pygame?
+    pygame.init
+What is the purpose of the "While running" loop?
+    to keep the program from ending and closing
+How do you create a screen in pygame?
+    screen = pygame.display.set_mode((800,600))
+How are objects placed on the screen in pygame?
+    screen.blit or similar methods
+What events can I listen for in pygame? What do those events do?
+    Quit: If game is closed
+    KeyDown: If a key is pushed
+    KeyUP: If a key is released
+How can I detect collision with pygame?
+    the distance formula
+How do you add sounds in pygame?
+    pygame.mixer
+'''
